de397342ffff3e1dac604b348aa2cb79f920d35d
Dorette <dorette.schutte86@gmail.com>
2023-08-27 10:54:07 +0200

    lesson20

---
+++ Classwork/lesson20/recipe_app_20_3/controllers/usersController.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const User = require("../models/user");
+
+module.exports = {
+  index: (req, res, next) => {
+    User.find()
+      .then(users => {
+        res.locals.users = users;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching users: ${error.message}`);
+        next(error);
+      });
+  },
+  indexView: (req, res) => {
+    res.render("users/index");
+  },
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) => {
+    let userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last
+      },
+      email: req.body.email,
+      password: req.body.password,
+      zipCode: req.body.zipCode
+    };
+    User.create(userParams)
+      .then(user => {
+        res.locals.redirect = "/users";
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving user: ${error.message}`);
+        next(error);
+      });
+  },
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+  show: (req, res, next) => {
+    let userId = req.params.id;
+    User.findById(userId)
+      .then(user => {
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  showView: (req, res) => {
+    res.render("users/show");
+  },
+  //listing 20.6
+  edit: (req, res, next) => { //add the edit action
+    let userId = req.params.id;
+    User.findById(userId) //use findById to locate a user in the database by their ID
+      .then(user => {
+        res.render("users/edit", {
+          user: user
+        }); //render the user edit page for a specific user in the database
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  update: (req, res, next) => { //add the update action
+    let userId = req.params.id,
+      userParams = {
+        name: {
+          first: req.body.first,
+          last: req.body.last
+        },
+        email: req.body.email,
+        password: req.body.password,
+        zipCode: req.body.zipCode
+      };  //collect user parameters from request
+    User.findByIdAndUpdate(userId, {
+      $set: userParams
+    })  //use findByIdAndUpdate to locate a user by ID and update the document record in one command
+      .then(user => {
+        res.locals.redirect = `/users/${userId}`;
+        res.locals.user = user;
+        next(); //add user to response as a local variable and call the next middleware function
+      })
+      .catch(error => {
+        console.log(`Error updating user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  //listing 20.9
+  delete: (req, res, next) => {
+    let userId = req.params.id;
+    User.findByIdAndRemove(userId)  //deleting a user with the findByIdAndRemove method
+      .then(() => {
+        res.locals.redirect = "/users";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting user by ID: ${error.message}`);
+        next();
+      });
+  }
+};
